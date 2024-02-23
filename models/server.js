7f87b5f8b5a5d43522916670ef8767645bd3c254
arodriguez-20199620 <Admin@ITLA0002.metroproyectos.local>
2024-02-23 09:15:19 -0600

    Middlewares configurados para las rutas

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { dbConnection } = require('../db/config');
+const cors = require('cors');
 
 class Server{
 
@@ -8,11 +9,18 @@ class Server{
         this.port = process.env.PORT;
 
         this.conectarDB();
+        this.middlewares();
 
     }
 
     async conectarDB(){
         await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
     }
 
     listen(){
