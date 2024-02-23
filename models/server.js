a9c331fe38688b4cfe2edd462da41e9f1c0b4f0c
arodriguez-20199620 <Admin@ITLA0002.metroproyectos.local>
2024-02-23 09:07:50 -0600

    Configuracion del server y escucha en el puerto adecuado

---
+++ models/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor ejecutado y escuchando en el puerto', this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
