8923f87532b42ed59626bf34105424a1b662104e
arodriguez-20199620 <Admin@ITLA0002.metroproyectos.local>
2024-02-23 09:13:28 -0600

    Conexion a base de datos MongoDB

--- models/server.js
+++ models/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { dbConnection } = require('../db/config');
 
 class Server{
 
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+
+        this.conectarDB();
+
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     listen(){
